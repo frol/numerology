8f2da528241106e00f2ed2698108d029f302a45d
Vlad Frolov <frolvlad@gmail.com>
2018-09-03 23:22:01 +0300

    Added TotalsInfo which represents the information about the whole text

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
-const LANGUAGES = {
-  Українська: {
-    alphabet: 'абвгґдеєжзиіїйклмнопрстуфхцчшщьюя',
-    vowels: 'аеєиіїоуюя',
-  },
-  Русский: {
-    alphabet: 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя',
-    vowels: 'аеёиоуыэюя',
-  },
-  English: {
-    alphabet: 'abcdefghijklmnopqrstuvwxyz',
-    vowels: 'aeiou',
-  },
+function sum(arr) {
+  return arr.reduce((acc, value) => acc + value, 0)
+}
+
+function last(arr) {
+  if (arr.length < 1) {
+    return null
+  }
+  return arr[arr.length - 1]
 }
 
 function reduceToDigit(value) {
@@ -26,23 +22,49 @@ function reduceToDigit(value) {
   return [oldValue].concat(reduceToDigit(newValue))
 }
 
-function letterToNumber(alphabet, letter) {
-  const letterIndex = alphabet.indexOf(letter)
-  if (letterIndex < 0) {
-    return 0
-  }
-  return (letterIndex % 9) + 1
+function displayAsChain(arr) {
+  return arr.join(' → ')
 }
 
-function textToNumbers(alphabet, text) {
-  const textNumber = text
-    .split('')
-    .map(letter => letterToNumber(alphabet, letter))
-    .reduce((acc, value) => acc + value, 0)
-  if (textNumber <= 0) {
-    return []
+class Language {
+  constructor({ alphabet, vowels }) {
+    this.alphabet = alphabet
+    this.vowels = vowels
   }
-  return reduceToDigit(textNumber)
+
+  isVowel = letter => this.vowels.indexOf(letter.toLowerCase()) >= 0
+  isConsonant = letter => this.alphabet.indexOf(letter.toLowerCase()) >= 0 && !this.isVowel(letter)
+
+  letterToNumber = letter => {
+    const letterIndex = this.alphabet.indexOf(letter.toLowerCase())
+    if (letterIndex < 0) {
+      return 0
+    }
+    return (letterIndex % 9) + 1
+  }
+
+  textToNumbers = text => {
+    const textNumber = sum(text.split('').map(letter => this.letterToNumber(letter)))
+    if (textNumber <= 0) {
+      return []
+    }
+    return reduceToDigit(textNumber)
+  }
+}
+
+const LANGUAGES = {
+  Українська: new Language({
+    alphabet: 'абвгґдеєжзиіїйклмнопрстуфхцчшщьюя',
+    vowels: 'аеєиіїоуюя',
+  }),
+  Русский: new Language({
+    alphabet: 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя',
+    vowels: 'аеёиоуыэюя',
+  }),
+  English: new Language({
+    alphabet: 'abcdefghijklmnopqrstuvwxyz',
+    vowels: 'aeiou',
+  }),
 }
 
 class LettersInfo extends React.Component {
@@ -51,9 +73,7 @@ class LettersInfo extends React.Component {
     if (word.length <= 0) {
       return null
     }
-    const { alphabet, vowels } = LANGUAGES[this.props.language]
-    const isVowel = letter => vowels.indexOf(letter) >= 0
-    const isConsonant = letter => alphabet.indexOf(letter) >= 0 && !isVowel(letter)
+    const language = LANGUAGES[this.props.language]
 
     return (
       <table className="letters-table">
@@ -62,8 +82,8 @@ class LettersInfo extends React.Component {
             {word.split('').map((letter, index) => (
               <th
                 key={`${index}-${letter}`}
-                className={`${isVowel(letter) ? 'vowel' : ''} ${
-                  isConsonant(letter) ? 'consonant' : ''
+                className={`${language.isVowel(letter) ? 'vowel' : ''} ${
+                  language.isConsonant(letter) ? 'consonant' : ''
                 }`}
               >
                 {letter}
@@ -79,32 +99,98 @@ class LettersInfo extends React.Component {
             {word.split('').map((letter, index) => (
               <td
                 key={`${index}-${letter}`}
-                className={`${isVowel(letter) ? 'vowel' : ''} ${
-                  isConsonant(letter) ? 'consonant' : ''
+                className={`${language.isVowel(letter) ? 'vowel' : ''} ${
+                  language.isConsonant(letter) ? 'consonant' : ''
                 }`}
               >
-                {letterToNumber(alphabet, letter)}
+                {language.letterToNumber(letter)}
               </td>
             ))}
             <td className="total vowel">
-              {textToNumbers(
-                alphabet,
-                word
-                  .split('')
-                  .filter(isVowel)
-                  .join('')
-              ).join(' → ')}
+              {displayAsChain(
+                language.textToNumbers(
+                  word
+                    .split('')
+                    .filter(language.isVowel)
+                    .join('')
+                )
+              )}
             </td>
             <td className="total consonant">
-              {textToNumbers(
-                alphabet,
-                word
-                  .split('')
-                  .filter(isConsonant)
-                  .join('')
-              ).join(' → ')}
+              {displayAsChain(
+                language.textToNumbers(
+                  word
+                    .split('')
+                    .filter(language.isConsonant)
+                    .join('')
+                )
+              )}
             </td>
-            <td className="total">{textToNumbers(alphabet, word).join(' → ')}</td>
+            <td className="total">{displayAsChain(language.textToNumbers(word))}</td>
+          </tr>
+        </tbody>
+      </table>
+    )
+  }
+}
+
+class TotalsInfo extends React.Component {
+  render() {
+    const language = LANGUAGES[this.props.language]
+    const { words } = this.props
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th className="vowel">Гласные</th>
+            <th className="consonant">Согласные</th>
+            <th>Итог</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              {displayAsChain(
+                reduceToDigit(
+                  sum(
+                    words.map(word =>
+                      last(
+                        language.textToNumbers(
+                          word
+                            .split('')
+                            .filter(language.isVowel)
+                            .join('')
+                        )
+                      )
+                    )
+                  )
+                )
+              )}
+            </td>
+            <td>
+              {displayAsChain(
+                reduceToDigit(
+                  sum(
+                    words.map(word =>
+                      last(
+                        language.textToNumbers(
+                          word
+                            .split('')
+                            .filter(language.isConsonant)
+                            .join('')
+                        )
+                      )
+                    )
+                  )
+                )
+              )}
+            </td>
+            <td>
+              {displayAsChain(
+                reduceToDigit(sum(words.map(word => last(language.textToNumbers(word)))))
+              )}
+            </td>
           </tr>
         </tbody>
       </table>
@@ -114,11 +200,13 @@ class LettersInfo extends React.Component {
 
 class WordsInfo extends React.Component {
   render() {
+    const words = this.props.text.split(' ')
     return (
       <>
-        {this.props.text.split(' ').map(word => (
+        {words.map(word => (
           <LettersInfo key={word} language={this.props.language} word={word} />
         ))}
+        <TotalsInfo language={this.props.language} words={words} />
       </>
     )
   }
@@ -131,7 +219,7 @@ export default class extends React.Component {
   }
 
   componentWillMount() {
-    if ('serviceWorker' in navigator) {
+    if (typeof navigator !== 'undefined' && 'serviceWorker' in navigator) {
       window.addEventListener('load', function() {
         navigator.serviceWorker
           .register('/sw.js')
